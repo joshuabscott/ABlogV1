3e0fc0fe3699a2a6f99b1eb0e7d73eec00f11a5b
Josh Scott <60557820+joshuabscott@users.noreply.github.com>
2021-07-09 16:24:44 -0700

    Initial commit

---
+++ Enums/ModerationType.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ABlogV1.Enums
+{
+    public enum ModerationType
+    {
+        [Description("Political Incorrect Fact")]
+        Political,
+        [Description("Offensive Language")]
+        Language,
+        [Description("Addictive Drug Use Promotion")]
+        Drugs,
+        [Description("Threatening Speech")]
+        Threatening,
+        [Description("Sexual Content")]
+        Sexual,
+        [Description("Hate Speech")]
+        HateSpeech,
+        [Description("Targeted Shaming")]
+        Shaming
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# ABlogV1
+ New Blog for .Net 5 using MVC - The Prometheus Agency
